51a52893d24a0b5aebf353664cc744fe99de85b9
Ben Koska <ben@koska.at>
2019-09-20 12:04:32 +0200

    Clean up Package.swift

--- Package.swift
+++ Package.swift
@@ -2,10 +2,28 @@
 
 import PackageDescription
 
-let package = Package(name: "LaunchAtLogin",
-                      products: [.library(name: "LaunchAtLogin",
-                                          targets: ["LaunchAtLogin", "LaunchAtLoginHelper"])],
-                      targets: [.target(name: "LaunchAtLogin",
-                                        path: "LaunchAtLogin"),
-                                        .target(name: "LaunchAtLoginHelper",
-                                        path: "LaunchAtLoginHelper")])
+let package = Package(
+	name: "LaunchAtLogin",
+	platforms: [
+		.macOS(.v10_12)
+	],
+	products: [
+		.library(
+			name: "LaunchAtLogin",
+			targets: [
+				"LaunchAtLogin",
+				"LaunchAtLoginHelper"
+			]
+		)
+	],
+	targets: [
+		.target(
+			name: "LaunchAtLogin",
+			path: "LaunchAtLogin"
+		),
+		.target(
+			name: "LaunchAtLoginHelper",
+			path: "LaunchAtLoginHelper"
+		)
+	]
+)
