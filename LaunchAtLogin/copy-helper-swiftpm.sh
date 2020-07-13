a33aeed5568fcf15cb792092df1620b8a173fd9e
Sergey Kuryanov <sergey.kuryanov@gmail.com>
2020-07-14 00:06:21 +0300

    Update helper script

--- LaunchAtLogin/copy-helper-swiftpm.sh
+++ LaunchAtLogin/copy-helper-swiftpm.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-origin_helper_path="$BUILT_PRODUCTS_DIR/LaunchAtLoginHelper.app"
-helper_dir="$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Library/LoginItems"
-helper_path="$helper_dir/$PRODUCT_BUNDLE_IDENTIFIER-LaunchAtLoginHelper.app"
-entitlements_path="$BUILT_PRODUCTS_DIR/LaunchAtLogin_LaunchAtLogin.bundle/Contents/Resources"
+package_resources_path="$BUILT_PRODUCTS_DIR/LaunchAtLogin_LaunchAtLogin.bundle/Contents/Resources"
 
-rm -rf "$helper_path"
-mkdir -p "$helper_dir"
-cp -rf "$origin_helper_path" "$helper_dir/"
+helper_name="LaunchAtLoginHelper.app"
+helper_path="$package_resources_path/$helper_name"
 
-defaults write "$helper_path/Contents/Info" CFBundleIdentifier -string "$PRODUCT_BUNDLE_IDENTIFIER-LaunchAtLoginHelper"
+contents_path="$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH"
+login_items="$contents_path/Library/LoginItems"
+login_helper_path="$login_items/$helper_name"
+
+rm -rf "$login_helper_path"
+mkdir -p "$login_items"
+cp -rf "$helper_path" "$login_helper_path"
+
+defaults write "$login_helper_path/Contents/Info" CFBundleIdentifier -string "$PRODUCT_BUNDLE_IDENTIFIER-LaunchAtLoginHelper"
 
 if [[ -n $CODE_SIGN_ENTITLEMENTS ]]; then
-    codesign --force --entitlements="$entitlements_path/LaunchAtLogin.entitlements" --options=runtime --sign="$EXPANDED_CODE_SIGN_IDENTITY_NAME" "$helper_path"
+    codesign --force --entitlements="$package_resources_path/LaunchAtLogin.entitlements" --options=runtime --sign="$EXPANDED_CODE_SIGN_IDENTITY_NAME" "$login_helper_path"
 else
     codesign --force --options=runtime --sign="$EXPANDED_CODE_SIGN_IDENTITY_NAME" "$helper_path"
 fi
 
 if [[ $CONFIGURATION == "Release" ]]; then
-    rm -rf "$origin_helper_path"
-    rm "$entitlements_path/copy-helper.sh"
-    rm "$entitlements_path/LaunchAtLogin.entitlements"
+    rm -rf "$contents_path/Resources/LaunchAtLogin_LaunchAtLogin.bundle"
 fi
