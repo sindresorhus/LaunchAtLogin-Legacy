cd05f7bcac64ccd6ff697ab0d8fb953c980d69e2
Sergey Kuryanov <sergey.kuryanov@gmail.com>
2020-07-15 01:39:29 +0300

    Add KVO compliance

--- LaunchAtLogin/LaunchAtLogin.swift
+++ LaunchAtLogin/LaunchAtLogin.swift
@@ -1,27 +1,30 @@
 import Foundation
 import ServiceManagement
 
-public class LaunchAtLogin {
+public class LaunchAtLogin: NSObject {
     public static let shared = LaunchAtLogin()
 
     public static var isEnabled: Bool {
         get { shared.isEnabled }
         set { shared.isEnabled = newValue }
     }
+
     private let id = "\(Bundle.main.bundleIdentifier!)-LaunchAtLoginHelper"
 
-	public var isEnabled: Bool {
-		get {
-			guard let jobs = (SMCopyAllJobDictionaries(kSMDomainUserLaunchd).takeRetainedValue() as? [[String: AnyObject]]) else {
-				return false
-			}
+    @objc dynamic public var isEnabled: Bool {
+        get {
+            guard let jobs = (SMCopyAllJobDictionaries(kSMDomainUserLaunchd).takeRetainedValue() as? [[String: AnyObject]]) else {
+                return false
+            }
 
-			let job = jobs.first { $0["Label"] as! String == id }
+            let job = jobs.first { $0["Label"] as! String == id }
 
-			return job?["OnDemand"] as? Bool ?? false
-		}
-		set {
-			SMLoginItemSetEnabled(id as CFString, newValue)
-		}
-	}
+            return job?["OnDemand"] as? Bool ?? false
+        }
+        set {
+            willChangeValue(for: \.isEnabled)
+            SMLoginItemSetEnabled(id as CFString, newValue)
+            didChangeValue(for: \.isEnabled)
+        }
+    }
 }
