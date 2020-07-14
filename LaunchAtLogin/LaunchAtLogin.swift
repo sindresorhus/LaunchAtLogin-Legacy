a2fe8b813714815464781ef740c67ce79be810a2
Sergey Kuryanov <sergey.kuryanov@gmail.com>
2020-07-15 01:40:36 +0300

    Add Combine support

--- LaunchAtLogin/LaunchAtLogin.swift
+++ LaunchAtLogin/LaunchAtLogin.swift
@@ -1,3 +1,4 @@
+import Combine
 import Foundation
 import ServiceManagement
 
@@ -8,6 +9,14 @@ public class LaunchAtLogin: NSObject {
         get { shared.isEnabled }
         set { shared.isEnabled = newValue }
     }
+
+    @available(OSX 10.15, *)
+    public static var publisher: AnyPublisher<Bool, Never> = {
+        shared
+            .publisher(for: \.isEnabled)
+            .removeDuplicates()
+            .eraseToAnyPublisher()
+    }()
 
     private let id = "\(Bundle.main.bundleIdentifier!)-LaunchAtLoginHelper"
 
@@ -22,9 +31,16 @@ public class LaunchAtLogin: NSObject {
             return job?["OnDemand"] as? Bool ?? false
         }
         set {
+            if #available(OSX 10.15, *) {
+                objectWillChange.send()
+            }
+
             willChangeValue(for: \.isEnabled)
             SMLoginItemSetEnabled(id as CFString, newValue)
             didChangeValue(for: \.isEnabled)
         }
     }
 }
+
+@available(OSX 10.15, *)
+extension LaunchAtLogin: ObservableObject { }
