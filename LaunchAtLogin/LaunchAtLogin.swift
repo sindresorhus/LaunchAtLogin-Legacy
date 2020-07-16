59209a82889f75d1905e387bc10f091fb11ece77
Sergey Kuryanov <sergey.kuryanov@gmail.com>
2020-07-17 01:42:41 +0300

    Use CurrentValueSubject as publisher

--- LaunchAtLogin/LaunchAtLogin.swift
+++ LaunchAtLogin/LaunchAtLogin.swift
@@ -6,23 +6,22 @@ public class LaunchAtLogin {
     public static let observable = LaunchAtLogin()
     public static let kvo = observable.kvo
 
+    @available(macOS 10.15, *)
+    public static var publisher = observable.publisher
+
     public static var isEnabled: Bool {
         get { observable.isEnabled }
         set { observable.isEnabled = newValue }
     }
 
-    @available(macOS 10.15, *)
-    public static var publisher: AnyPublisher<Bool, Never> = {
-        LaunchAtLogin
-            .kvo
-            .publisher(for: \.isEnabled)
-            .removeDuplicates()
-            .eraseToAnyPublisher()
-    }()
-
     private let id = "\(Bundle.main.bundleIdentifier!)-LaunchAtLoginHelper"
 
     public lazy var kvo = LaunchAtLoginKVO(launchAtLogin: self)
+
+    @available(macOS 10.15, *)
+    private lazy var _publisher = CurrentValueSubject<Bool, Never>(isEnabled)
+    @available(macOS 10.15, *)
+    public lazy var publisher = _publisher.eraseToAnyPublisher()
 
     public var isEnabled: Bool {
         get {
@@ -42,6 +41,10 @@ public class LaunchAtLogin {
             kvo.willChangeValue(for: \.isEnabled)
             SMLoginItemSetEnabled(id as CFString, newValue)
             kvo.didChangeValue(for: \.isEnabled)
+
+            if #available(macOS 10.15, *) {
+                _publisher.send(newValue)
+            }
         }
     }
 }
