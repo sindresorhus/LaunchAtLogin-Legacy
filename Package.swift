d0a5b31ca4e2cb89856933c615cc40d759e058e5
Sergey Kuryanov <sergey.kuryanov@gmail.com>
2020-08-03 13:37:46 +0300

    Use Tab indentation for Package.swift

--- Package.swift
+++ Package.swift
@@ -2,29 +2,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "LaunchAtLogin",
-    platforms: [
-        .macOS(.v10_12)
-    ],
-    products: [
-        .library(
-            name: "LaunchAtLogin",
-            targets: ["LaunchAtLogin"]
-        )
-    ],
-    targets: [
-        .target(
-            name: "LaunchAtLogin",
-            exclude: [
-                "Info.plist",
-                "copy-helper.sh"
-            ],
-            resources: [
-                .copy("LaunchAtLogin.entitlements"),
-                .copy("copy-helper-swiftpm.sh"),
-                .copy("LaunchAtLoginHelper.zip"),
-                .copy("LaunchAtLoginHelper-with-runtime.zip")
-            ]
-        )
-    ]
+	name: "LaunchAtLogin",
+	platforms: [
+		.macOS(.v10_12)
+	],
+	products: [
+		.library(
+			name: "LaunchAtLogin",
+			targets: ["LaunchAtLogin"]
+		)
+	],
+	targets: [
+		.target(
+			name: "LaunchAtLogin",
+			exclude: [
+				"Info.plist",
+				"copy-helper.sh"
+			],
+			resources: [
+				.copy("LaunchAtLogin.entitlements"),
+				.copy("copy-helper-swiftpm.sh"),
+				.copy("LaunchAtLoginHelper.zip"),
+				.copy("LaunchAtLoginHelper-with-runtime.zip")
+			]
+		)
+	]
 )
