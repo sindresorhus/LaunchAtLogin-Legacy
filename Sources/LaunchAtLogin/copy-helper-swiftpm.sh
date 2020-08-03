4921e4f389c738d6c6a18128e350dd93dfcfba6d
Sergey Kuryanov <sergey.kuryanov@gmail.com>
2020-08-03 16:50:06 +0300

    Improve copy-helper-swiftpm script

--- Sources/LaunchAtLogin/copy-helper-swiftpm.sh
+++ Sources/LaunchAtLogin/copy-helper-swiftpm.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+HELPER_CHECKSUM="4842fa690b83d96d509dad6763571125e4a2ae2dcf8f858f88518486816a5598"
+HELPER_CHECKSUM_RUNTIME="ceef772a05157e9b64e40fb022d19e6462e371f9d69d1dbd3c1e64096bde535d"
+
 verlte() {
 	[  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
 }
 
 if verlte "10.14.4" "$MACOSX_DEPLOYMENT_TARGET"; then
 	helper_name="LaunchAtLoginHelper"
-	checksum="4842fa690b83d96d509dad6763571125e4a2ae2dcf8f858f88518486816a5598"
+	checksum="$HELPER_CHECKSUM"
 else
 	helper_name="LaunchAtLoginHelper-with-runtime"
-	checksum="ceef772a05157e9b64e40fb022d19e6462e371f9d69d1dbd3c1e64096bde535d"
+	checksum="$HELPER_CHECKSUM_RUNTIME"
 fi
 
 package_resources_path="$BUILT_PRODUCTS_DIR/LaunchAtLogin_LaunchAtLogin.bundle/Contents/Resources"
@@ -24,9 +27,9 @@ rm -rf "$login_helper_path"
 mkdir -p "$login_items"
 
 # Verify SHA256 checksum of LaunchAtLoginHelper
-helper_checksum="$(shasum -a 256 "$helper_path" | awk '{print $1}')"
+zip_checksum="$(shasum -a 256 "$helper_path" | awk '{print $1}')"
 
-if [[ "$helper_checksum" != "$checksum" ]]; then
+if [[ "$zip_checksum" != "$checksum" ]]; then
 	echo "Wrong checksum of LaunchAtLoginHelper"
 	exit 1
 fi
