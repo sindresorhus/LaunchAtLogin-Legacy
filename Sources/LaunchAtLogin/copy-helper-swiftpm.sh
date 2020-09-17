7f469e45119c1eaa8415ca9a731baf6240182b35
Sindre Sorhus <sindresorhus@gmail.com>
2020-09-17 17:30:57 +0200

    Minor tweaks

--- Sources/LaunchAtLogin/copy-helper-swiftpm.sh
+++ Sources/LaunchAtLogin/copy-helper-swiftpm.sh
@@ -4,7 +4,7 @@ HELPER_CHECKSUM="6eaaced9173120f82ef98452b2d8cb3705c12db39d4286c70c4f5bb6b67a5e4
 HELPER_CHECKSUM_RUNTIME="c5a48f5ba681088aa7922b79f2f1b55a1a006904b8de378790eb0e8b0ac66234"
 
 verlte() {
-	[  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+	[ "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
 }
 
 if verlte "10.14.4" "$MACOSX_DEPLOYMENT_TARGET"; then
@@ -26,7 +26,7 @@ login_helper_path="$login_items/LaunchAtLoginHelper.app"
 rm -rf "$login_helper_path"
 mkdir -p "$login_items"
 
-# Verify SHA256 checksum of LaunchAtLoginHelper
+# Verify SHA256 checksum of LaunchAtLoginHelper.
 zip_checksum="$(shasum -a 256 "$helper_path" | awk '{print $1}')"
 
 if [[ "$zip_checksum" != "$checksum" ]]; then
