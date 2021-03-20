97195d2cad014cdfceb9080428f6d22c6d966dcf
Sindre Sorhus <sindresorhus@gmail.com>
2021-03-20 15:58:49 +0700

    Update copy-helper-swiftpm.sh

--- Sources/LaunchAtLogin/copy-helper-swiftpm.sh
+++ Sources/LaunchAtLogin/copy-helper-swiftpm.sh
@@ -44,7 +44,7 @@ else
 	codesign --force --deep --options=runtime --sign="$EXPANDED_CODE_SIGN_IDENTITY_NAME" "$helper_path"
 fi
 
-# If this is being built for multiple architectures, assume it is a release build and we should clean up
+# If this is being built for multiple architectures, assume it is a release build and we should clean up.
 if [[ $ONLY_ACTIVE_ARCH == "NO" ]]; then
 	rm -rf "$contents_path/Resources/LaunchAtLogin_LaunchAtLogin.bundle"
 fi
